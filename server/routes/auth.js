2a1cece7f641786ef0cbfe581ce916b3ec996bd4
Alex Scott <axs.scott@gmail.com>
2022-05-10 00:17:18 -0400

    user lockout implemented

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,9 +5,24 @@ const User          = require('../models/user.model');
 const bcrypt        = require('bcrypt');
 const passport      = require('passport');
 const session       = require('express-session');
+const moment = require('moment');
+const ExpressBrute = require('express-brute');
+const store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
 let message = '';
 
+const lockoutCallback = function(req, res, next, nextValidRequestDate) {
+    res.status(429).send({ status: 'lockedout', error: 'Too many attempts, please try again later' });
+    // logger.auth.info(`Lockout: ${req.ip} Next Valid: ${nextValidRequestDate}`);
+};
+ 
 
+// Start slowing requests after 5 failed attempts to do something for the same user
+const userBruteforce = new ExpressBrute(store, {
+    freeRetries: 1,
+    minWait: 1*60*1000, // 5 minutes
+    maxWait: 60*60*1000, // 1 hour,
+    failCallback: lockoutCallback,
+});
 
 router.post('/', async(req, res) =>{
     if (req.session.user && req.cookies.user_sid) {
@@ -65,16 +80,26 @@ router.post('/logout', (req, res) =>{
     res.json('Logged out')
 })
 
-router.post('/login',passport.authenticate('local', { failWithError: true }),
-  function(req, res, next) {
-    req.session.user = req.user.username;
-    res.json('success');
-  },
-  function(err, req, res, next) {
-    // handle error
-    console.log(err);
-    res.json(err);
-  }
+router.post('/login', userBruteforce.prevent, passport.authenticate('local', { failWithError: true }),
+
+    function(req, res, next) {
+        req.session.user = req.user.username;
+        req.brute.reset();
+        if (!req.user.locked_out) {
+            const currentTimestamp = moment().unix(); // in seconds
+            const currentDatetime = moment(currentTimestamp * 1000).format(
+                    'YYYY-MM-DD HH:mm:ss'
+            );
+            res.status(200).send({ data: 'success', user: req.user, time: currentDatetime });
+        } else {
+            res.status(202).send({ data: 'lockedout', user: req.user });
+        }
+    },
+    function(err, req, res, next) {
+        // handle error
+        console.log(err);
+        res.json(err);
+    }
 );
 
 module.exports = router;
